1662a59a18ce1364eb821c0a7c10f4c0486fb5d5
MalekMohanna <malekmhana@gmail.com>
2022-10-02 16:38:10 +0300

    Validation

--- Tv_Shows/FirstApp/models.py
+++ Tv_Shows/FirstApp/models.py
@@ -1,7 +1,18 @@
-from email.policy import default
 from django.db import models
 
 # Create your models here.
+
+class ShowManager(models.Manager):
+    def validator(self,data):
+        errors={}
+        if len(data['show-t']) < 2 :
+            errors['title']='Title should be at least 2 characters'
+        if len(data['netw']) < 3 :
+            errors['network']='Network should be at least 3 characters'
+        if len(data['descr']) < 10 :
+            errors['descreption']='Descreption should be at least 10 characters'
+        return errors
+
 class Show(models.Model):
     title=models.CharField(max_length=255)
     network=models.CharField(max_length=255)
@@ -9,3 +20,5 @@ class Show(models.Model):
     release_date=models.DateField(null=True)
     created_at=models.DateTimeField(auto_now_add = True)
     updated_at=models.DateTimeField(auto_now = True)
+    objects= ShowManager()
+
--- Tv_Shows/FirstApp/views.py
+++ Tv_Shows/FirstApp/views.py
@@ -1,42 +1,45 @@
 from multiprocessing import context
 from django.shortcuts import render,redirect
 from .models import Show
+from django.contrib import messages
 
 def index(request):
     return redirect('/shows')
 
 def root(request):
-    context={
-        'my_shows': Show.objects.all()
-    }
+    context={'my_shows': Show.objects.all()}
     return render(request,'index.html',context)
 
 def new(request):
     return render(request,'new.html')
 
 def create(request):
-    x = Show.objects.create(title=request.POST['show-t'],network=request.POST['netw'],release_date=request.POST['release-d'],descreption=request.POST['descr'])
-    x.save()
-    id=str(Show.objects.last().id)
-    return redirect('/shows/'+id)
+    if request.method=="POST":
+        errors = Show.objects.validator(request.POST)
+        if len(errors)>0:
+            for key, value  in errors.items():
+                messages.error(request, value)
+            return redirect('/shows/new')
+        else :
+            x = Show.objects.create(title=request.POST['show-t'],network=request.POST['netw'],release_date=request.POST['release-d'],descreption=request.POST['descr'])
+            x.save()
+            id=str(Show.objects.last().id)
+            x=request.POST
+            return redirect('/shows/'+id)
 
 def shows(request,id):
     z=id
-    context={
-        'created_show':Show.objects.get(id=z)
-    }
+    context={'created_show':Show.objects.get(id=z)}
     return render(request,'show.html',context)
 
 def edit(request,id):
     context={
-        'my_id':str(id),
         'my_show':Show.objects.get(id=id),
         'show_release':Show.objects.get(id=id).release_date.strftime('%Y-%m-%d')
     }
     return render(request,'edit.html',context)
 
 def update(request,id):
-    print('hiiii')
     my_id=id
     to_update=Show.objects.get(id=my_id)
     to_update.title=request.POST['show-t']
